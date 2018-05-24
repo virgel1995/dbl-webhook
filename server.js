8eb5522bff1007e5ed663ae117f6aa10c4ef92eb
Glitch (dbl-webhook-test) <none>
2018-05-24 08:17:43 +0000

    🎡☄️ Checkpoint ./server.js:626605/1247 ./package.json:626605/461

--- server.js
+++ server.js
@@ -2,22 +2,28 @@ var express = require('express');
 var snekfetch = require('snekfetch')
 var app = express();
 
+
+var webhookurl = "https://discordapp.com/api/webhooks/445111925177712655/G3m0xTE73LqYi2o_Nrbja6psez_BRbJ4GgR0-PJPzeJLwdnR2xpJsLymYd5SB4l_ApYB"
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.send("dis is not a website kthx")
 });
 
 app.post("/hook", function (req, res) {
-  if (req.headers.authorization !== "lmao") return res.send("BAAD")
+  if (req.headers.authorization !== "lmao") return res.send({code: "invalid auth"})
     var user_id = req.body.user;
     var bot = req.body.bot;
-snekfetch("https://discordapp.com/api/webhooks/445111925177712655/G3m0xTE73LqYi2o_Nrbja6psez_BRbJ4GgR0-PJPzeJLwdnR2xpJsLymYd5SB4l_ApYB")
-    res.send("good");
+  if (req.body.type === "test") {
+    snekfetch.post(webhookurl).send({ "content": `<@${user_id}> voted <@${bot}> (This is a test)`}).then(r => {})
+  } else {
+    snekfetch.post(webhookurl).send({ "content": `<@${user_id}> voted <@${bot}>`}).then(r => {})
+}
+    res.send({code: "success"});
 });
 
 var listener = app.listen(process.env.PORT, function () {
