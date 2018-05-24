fd08df7b08116a9a00eae6963336267368bde549
Glitch (dbl-webhook-test) <none>
2018-05-24 08:07:22 +0000

    🚶🍁 Checkpoint ./server.js:626605/2034 ./package.json:626605/1283

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
+var snekfetch = require('snekfetch')
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
+app.post("/hook", function (req, res) {
+  if (req.headers.authorization !== "lmao") return res.send("BAAD")
+    var user_id = req.body.user;
+    var bot = req.body.bot;
+snekfetch("https://discordapp.com/api/webhooks/445111925177712655/G3m0xTE73LqYi2o_Nrbja6psez_BRbJ4GgR0-PJPzeJLwdnR2xpJsLymYd5SB4l_ApYB")
+    res.send("good");
+});
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
